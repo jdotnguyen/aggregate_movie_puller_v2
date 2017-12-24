423a042dd0e04f377de71dd97633b19ac097feb0
Justin Nguyen <justin@gfisystems.ca>
2017-12-24 12:39:01 -0500

    Adjustments to padding/margin, "Known for" section added to actors page

--- assets/js/CastDetailsManager.js
+++ assets/js/CastDetailsManager.js
@@ -8,12 +8,12 @@ function CastDetailsManager () {
 	};
 
 	//Populate Cast Details
-	this.castDetailsAjax = function (type) {
+	this.castDetailsAjax = function (pid) {
 		//Style header
   		$('.header-title').html("Cast Details");
 
 	  	//Find out what type of call we're making
-	  	var ajaxUrl = "https://api.themoviedb.org/3/person/" + type + "?api_key=7daac836cb57a577537d2d5ba0313dc8&language=en-US&page=1";
+	  	var ajaxUrl = "https://api.themoviedb.org/3/person/" + pid + "?api_key=7daac836cb57a577537d2d5ba0313dc8&language=en-US&page=1";
 
 	  	//API call
 	  	$.ajax({
@@ -62,6 +62,45 @@ function CastDetailsManager () {
           		$(clone).click(function() {
             		movie_details_manager.movieDetailHandler(mid);
           		});
+
+          		//Get known movies ajax
+		      	$.ajax({
+		      		url: "https://api.themoviedb.org/3/person/" + pid + "/movie_credits?api_key=7daac836cb57a577537d2d5ba0313dc8&language=en-US",
+		        	contentType: "application/json",
+		        	type: "GET",
+		        	crossDomain: true,
+		        	success: function(data) {
+			          	//Get each similar movie
+			          	var results = data.cast;
+
+			          	//Pop in each similar movie entry
+			          	$.each(results, function (i, v) {
+			          		var movie_details_known_for_template = $('#cast-details-known-for-template').html();
+			          		var movie_details_known_for_template_clone = $(movie_details_known_for_template).clone();
+			          		var mid = this.id;
+
+			          		//Populate movie poster and title into template
+			          		$(movie_details_known_for_template_clone).find('.movie-poster').attr('src', 'https://image.tmdb.org/t/p/w300_and_h450_bestv2/' + this.poster_path);
+			          		$(movie_details_known_for_template_clone).find('.cast-details-known-for-movie-title').html(this.title);
+
+			          		//Append to main body
+			          		$(clone).find('.movie-details-known-for-list').append(movie_details_known_for_template_clone);
+
+			          		//Click handler for each of similar movie
+			          		$(movie_details_known_for_template_clone).click(function () {
+			          			movie_details_manager.movieDetailHandler(mid);
+			          		});
+
+			          		//Only show top 8
+			          		if (i == 5) {
+			          			return false;
+			          		}
+			          	});
+		        	},
+		        	error: function(err) {
+		            	alert(JSON.stringify(err));
+		        	}
+		      	});
 	      	},
 	      	error: function(err) {
 	          	alert(JSON.stringify(err));
